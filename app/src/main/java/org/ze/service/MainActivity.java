0d9a7a69c4c727c737d953d9035921c108438f1a
rick99856 <s13113241@stu.edu.tw>
2016-05-25 11:44:46 +0800

    0525 home in onstop

--- app/src/main/java/org/ze/service/MainActivity.java
+++ app/src/main/java/org/ze/service/MainActivity.java
@@ -1,17 +1,28 @@
 package org.ze.service;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private Button startButton1;
     private Button stopButton;
 
+    public static boolean OnPause = false;
+    public static boolean OnResume = false;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         startButton1 = (Button) findViewById(R.id.start1);
         stopButton = (Button) findViewById(R.id.stop);
@@ -38,4 +49,44 @@ public class MainActivity extends Activity {
             stopService(intent);
         }
     };
+//    @Override
+//    public void onAttachedToWindow() {
+//        super.onAttachedToWindow();
+//        getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
+//        Log.d("mark", "onAttachedToWindow-- ");
+//    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        OnPause  = true;
+        Log.e("Onpause","onpause" + OnPause);
+
+    }
+    @Override
+    protected void onResume() {
+        // TODO Auto-generated method stub
+        super.onResume();
+
+        OnResume = true;
+        Log.e("Onresume","Onresume" + OnResume);
+
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("Onstop","Onpause is "+OnPause+" OnResume is "+OnResume);
+
+        if(OnPause == true && OnResume == true)
+        {
+            Log.e("My activity ", " **** home is press *** ");
+            //Do Your Home press code Here.
+            Toast.makeText(MainActivity.this,"home",Toast.LENGTH_LONG).show();
+
+        }
+        OnPause = false;
+        OnResume = false;
+        Log.e("stop","stop");
+    }
 }
