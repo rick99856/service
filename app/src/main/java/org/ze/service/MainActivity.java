d3999efa42c1342ccb239c1efd1839bff5b40b45
rick99856 <s13113241@stu.edu.tw>
2016-05-25 21:06:04 +0800

    0525 dialog

--- app/src/main/java/org/ze/service/MainActivity.java
+++ app/src/main/java/org/ze/service/MainActivity.java
@@ -1,26 +1,27 @@
 package org.ze.service;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity{
     private Button startButton1;
     private Button stopButton;
 
     public static boolean OnPause = false;
     public static boolean OnResume = false;
 
+
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
@@ -76,7 +77,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onStop() {
         super.onStop();
-        Log.e("Onstop","Onpause is "+OnPause+" OnResume is "+OnResume);
+        Log.e("Onstop", "Onpause is " + OnPause + " OnResume is " + OnResume);
 
         if(OnPause == true && OnResume == true)
         {
@@ -89,4 +90,7 @@ public class MainActivity extends Activity {
         OnResume = false;
         Log.e("stop","stop");
     }
+
+
+
 }
--- app/src/main/java/org/ze/service/NickyService.java
+++ app/src/main/java/org/ze/service/NickyService.java
@@ -1,12 +1,18 @@
 package org.ze.service;
 
+import android.app.AlertDialog;
+import android.app.KeyguardManager;
 import android.app.Service;
 import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.util.Log;
+import android.view.WindowManager;
 
 import java.util.Date;
 import java.util.List;
@@ -16,7 +22,7 @@ import java.util.TreeMap;
 //繼承android.app.Service
 public class NickyService extends Service {
     private Handler handler = new Handler();
-
+//    final AlertDialog alertDialog = getAlertDialog("這是一個對話框","請選擇......");
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -66,10 +72,85 @@ public class NickyService extends Service {
 
                 }
             }
-            Log.e("ss",foregroundProcess);
-            handler.postDelayed(this, 1000);
+            Log.e("ss", foregroundProcess);
+            if(foregroundProcess.equals("whitebird.ptt_now")){
+                String a = foregroundProcess;
+
+//                showDialog("ptt","ptt");
+                showAlertDialog();
+            }
+            else{
+                handler.postDelayed(this, 5000);
+            }
+
+
         }
     };
+
+
+    public void showDialog(String title,String message){
+        Log.i("service","show dialog function");
+        Log.i("service", "dialog error msg:" + message);
+//        errmsg.setText(Html.fromHtml(message));
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.setPositiveButton("不可以用", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);//設定提示框為系統提示框
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY);
+        alert.setCanceledOnTouchOutside(true);
+        alert.show();
+    }
+    private void showAlertDialog() {
+
+        KeyguardManager km = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        KeyguardManager.KeyguardLock kl = km.newKeyguardLock("MyKeyguardLock");
+        kl.disableKeyguard();
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK| PowerManager.ACQUIRE_CAUSES_WAKEUP
+                | PowerManager.ON_AFTER_RELEASE, "MyWakeLock");
+        wakeLock.acquire();
+
+
+//        final CharSequence[] items = { getString(R.string.test1), getString(R.string.tes2), getString(R.string.test3), getString(R.string.cancel) };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+        builder.setTitle("Title");
+
+        AlertDialog alert = builder.create();
+        alert.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
+        alert.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+
+        alert.show();
+
+        alert.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+            @Override
+            public void onDismiss(DialogInterface arg0) {
+
+            }
+        });
+
+
+        alert.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+            @Override
+            public void onCancel(DialogInterface dialog) {
+
+            }
+        });
+
+
+    }
 
 
 
